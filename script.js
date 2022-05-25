5aee442521379f7f4ab80a6dec4966d58d366072
Robson Arvelino <rob.arvelino@gmail.com>
2022-05-24 21:49:51 -0300

    24/05/2022

--- script.js
+++ script.js
@@ -4,58 +4,77 @@ let qtdep = 0
 let qtdel = 0
 let palavra =""
 let qtde = 0
-const conectivos = ["A","E","I","O","U"]
+let soma = 0
+const conectivos = ["de"]
 const btn = document.querySelector("#btn-calc")
 //let txt = texto.value
 btn.addEventListener("click", function(){
     let letras = texto.value.replace(/\s+/g,"") //retira os espaços da string
-    let palavras = texto.value.replace(/\s+/g,",")//substitui os espaços por virgula
-    let text = palavras.split(",")
+    let virgulas = texto.value.replace(/\.+/g,"")
+    virgulas = virgulas.replace(/\,+/g,"")
+    virgulas = virgulas.replace(/\;+/g,"")
+    virgulas = virgulas.replace(/\"+/g,"")
+    let palavras = virgulas.replace(/\s+/g,"--")//substitui os espaços por dois traços
+    
+    let text = palavras.split("--") //transforma a string em array
     qtdep = text.length
     qtdel = letras.length
-    let unique = separar(text)
+    let unique = tratar(text)
     printResul(unique)
-    
 })
+
 
 function printResul(matriz){
     res.innerHTML = ""
     let para = document.createElement("p")
     para.innerHTML = ` Palavras: ${qtdep}<br>`
     para.innerHTML += `Letras: ${qtdel}<br>`
-    for(let z in matriz){
-        para.innerHTML += `${matriz[z].palavra}: ${matriz[z].qtde}<br>`
+    if(matriz.length>10){
+        for(let z =0; z<10; z++){
+            para.innerHTML += `${matriz[z].palavra}: ${matriz[z].qtde} - ${matriz[z].porc}% <br>`
+        }
+    }else{
+        for(let z in matriz){
+            para.innerHTML += `${matriz[z].palavra}: ${matriz[z].qtde} - ${matriz[z].porc}% <br>`
+        }
     }
-      
-    res.appendChild(para)
-    
+          
+    res.appendChild(para)    
 }
 
-function separar(valor){
+function tratar(valor){
     let i = 0
     let unico = []
     let unitario =[]
 
-    for(let x in valor){         
+    for(let x in valor){ //array de palavras unicas 
         if(!unico.includes(valor[x])){
             unico[i]= valor[x]
             i++
         }                       
     }
     
-    for(let x in unico){
-        unitario[x] = {palavra:unico[x]}
+    for(let x in unico){//Monta o array unitario, inclue a palavra
+        if(unico[x] !="" && unico[x].length>2){
+            unitario[x] = {palavra:unico[x]}
+        }   
     }
 
-    for(let j in unitario){
-        let soma = 0
+    for(let j in unitario){//Monta o array unitario, inclue a quantidade
         for(let z in valor){
             if(unitario[j].palavra == valor[z]){
                 soma++               
             }
         }       
-        unitario[j].qtde = soma       
+        unitario[j].qtde = soma
+        unitario[j].porc = Math.round(soma/valor.length*100)
+        soma = 0
     }
+
+
+    unitario.sort(function(a,b){return b.qtde - a.qtde})// Classifica o array pela maior quantidade
+
     // alert(unitario[0].palavra)
     return unitario
 }
+
